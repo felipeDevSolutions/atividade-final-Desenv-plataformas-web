a8e2368c27e9872a9300944e61e2398ced355991
Felipe Costa <felipeejazz@gmail.com>
2024-09-09 22:42:07 -0300

    novo ajuste no código firebaseConfig

--- MyTask/backend/firebaseConfig.js
+++ MyTask/backend/firebaseConfig.js
@@ -1,4 +1,9 @@
 const admin = require('firebase-admin');
+const dotenv = require('dotenv'); 
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 //Para funcionar em ambiente de testes, reativar a linha abaixo e inativar a linha seguinte:
 // const serviceAccount = require('./firebase-service-account.json');
