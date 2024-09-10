08fb7e3b4c409c1c881e3c6583a27fd82bb2780f
Felipe Costa <felipeejazz@gmail.com>
2024-09-09 22:27:49 -0300

    Ajuste no código firebaseConfig para usar a variável de ambiente firebase-service-account na vercel

--- MyTask/backend/firebaseConfig.js
+++ MyTask/backend/firebaseConfig.js
@@ -1,5 +1,9 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
+
+//Para funcionar em ambiente de testes, reativar a linha abaixo e inativar a linha seguinte:
+// const serviceAccount = require('./firebase-service-account.json');
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
